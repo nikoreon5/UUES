0bb77f7eb25dfc27170f1455782902cae93fc0b5
nikoreon5 <164639526+nikoreon5@users.noreply.github.com>
2024-03-23 19:09:38 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 (function() {
 	console.log(Math.random());
+	console.log($('div[class="news-home-item "]:contains("Автор: Андреева")'));
 	$('div[class="news-home-item "]:contains("Автор: Андреева")').remove();
+	setTimeout(function () {
+		console.log($('div[class="news-home-item "]:contains("Автор: Андреева")'));
+	},1500);
 })();
