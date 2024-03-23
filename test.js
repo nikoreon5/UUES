efae5c17027badf2967d1ddf0ae052f85e816e52
nikoreon5 <164639526+nikoreon5@users.noreply.github.com>
2024-03-23 19:12:15 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 (function() {
 	console.log(Math.random());
-	console.log($('div[class="news-home-item "]:contains("Автор: Андреева")'));
+	console.log($('div[class="news-home-item "]:contains("Автор: Андреева")')[0]);
 	$('div[class="news-home-item "]:contains("Автор: Андреева")').remove();
 	setTimeout(function () {
-		console.log($('div[class="news-home-item "]:contains("Автор: Андреева")'));
+		console.log($('div[class="news-home-item "]:contains("Автор: Андреева")')[0]);
 	},1500);
 })();
